701bf0536999bc8eaa982865b0c4a7d3f63dd0cd
Bonono63 <bonoria63@gmail.com>
2024-05-09 11:07:29 -0400

    Add files via upload

---
+++ alacritty.bat
@@ -0,0 +1,7 @@
+cd %HOMEDRIVE%%HOMEPATH%
+
+set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\mingw64\bin\;
+set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\neovim\nvim-win64\bin\;
+set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\git\cmd\;
+
+start /B %HOMEDRIVE%%HOMEPATH%\portable\alacritty.exe --config-file %HOMEDRIVE%%HOMEPATH%\portable\alacritty.toml
--- setup.bat
+++ setup.bat
@@ -1,5 +1,6 @@
 doskey clear=cls
 doskey ls=dir
+doskey nvim=nvim -u init.lua
 doskey neovim=nvim
 doskey vim=nvim
 
@@ -10,10 +11,24 @@ mkdir portable
 cd portable
 set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\mingw64\bin\;
 set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\neovim\nvim-win64\bin\;
+set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\git\cmd\;
 echo Retrieving neovim
 IF NOT EXIST neovim.zip powershell.exe -command "Invoke-WebRequest https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-win64.zip -OutFile neovim.zip"
 echo Retrieving mingw
 IF NOT EXIST mingw64.7z powershell.exe -command "Invoke-WebRequest https://github.com/niXman/mingw-builds-binaries/releases/download/13.2.0-rt_v11-rev1/x86_64-13.2.0-release-win32-seh-msvcrt-rt_v11-rev1.7z -OutFile mingw64.7z"
+echo Retrieving Git
+IF NOT EXIST git.zip powershell.exe -command "Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.45.0.windows.1/MinGit-2.45.0-64-bit.zip -OutFile git.zip"
+echo Retrieving Alacritty
+IF NOT EXIST alacritty.exe powershell.exe -command "Invoke-WebRequest https://github.com/alacritty/alacritty/releases/download/v0.13.2/Alacritty-v0.13.2-portable.exe -OutFile alacritty.exe"
 echo Extracting archives
 IF NOT EXIST neovim\NUL powershell.exe -command "Expand-Archive neovim.zip"
+IF NOT EXIST git\NUL powershell.exe -command "Expand-Archive git.zip"
 IF NOT EXIST mingw64\NUL "C:\Program Files\7-Zip\7z.exe" x -y mingw64.7z
+
+cd %APPDATA%\..\Local\
+mkdir nvim
+cd nvim
+git clone https://github.com/Bonono63/nvim-config.git
+
+cd %HOMEDRIVE%%HOMEPATH%
+exit
