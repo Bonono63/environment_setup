d19f416a76d42ff66604768c69f9694fa4f34470
Bonono63 <bonoria63@gmail.com>
2024-12-10 12:29:56 -0500

    updated files

--- alacritty.bat
+++ alacritty.bat
@@ -3,5 +3,6 @@ cd %HOMEDRIVE%%HOMEPATH%
 set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\mingw64\bin\;
 set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\neovim\nvim-win64\bin\;
 set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\git\cmd\;
+set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\cmake\cmake-3.31.2-windows-x86_64\bin\;
 
 start /B %HOMEDRIVE%%HOMEPATH%\portable\alacritty.exe --config-file %HOMEDRIVE%%HOMEPATH%\portable\alacritty\alacritty.toml
--- setup.bat
+++ setup.bat
@@ -1,11 +1,15 @@
 REM C:\Windows\System32\cmd.exe /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" "C:\Windows\regedit.exe""
 
+echo @off
+
 cd %HOMEDRIVE%%HOMEPATH%
 mkdir portable
 cd portable
 set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\mingw64\bin\;
 set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\neovim\nvim-win64\bin\;
 set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\git\cmd\;
+set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\cmake\cmake-3.31.2-windows-x86_64\bin;
+
 echo Retrieving neovim
 IF NOT EXIST neovim.zip powershell.exe -command "Invoke-WebRequest https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-win64.zip -OutFile neovim.zip"
 echo Retrieving mingw
@@ -14,9 +18,14 @@ echo Retrieving Git
 IF NOT EXIST git.zip powershell.exe -command "Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.45.0.windows.1/MinGit-2.45.0-64-bit.zip -OutFile git.zip"
 echo Retrieving Alacritty
 IF NOT EXIST alacritty.exe powershell.exe -command "Invoke-WebRequest https://github.com/alacritty/alacritty/releases/download/v0.13.2/Alacritty-v0.13.2-portable.exe -OutFile alacritty.exe"
+echo Retrieving CMake
+IF NOT EXIST cmake.exe powershell.exe -command "Invoke-WebRequest https://github.com/Kitware/CMake/releases/download/v3.31.2/cmake-3.31.2-windows-x86_64.zip -OutFile cmake.zip"
+
+
 echo Extracting archives
 IF NOT EXIST neovim\NUL powershell.exe -command "Expand-Archive neovim.zip"
 IF NOT EXIST git\NUL powershell.exe -command "Expand-Archive git.zip"
+IF NOT EXIST cmake\NUL powershell.exe -command "Expand-Archive cmake.zip"
 IF NOT EXIST mingw64\NUL "C:\Program Files\7-Zip\7z.exe" x -y mingw64.7z
 
 cd %APPDATA%\..\Local\
