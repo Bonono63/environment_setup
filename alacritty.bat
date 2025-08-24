59495ae6fca0c4a93ca24a63dc38fb259f606a46
Bonono63 <bonoria63@gmail.com>
2025-08-24 15:41:26 -0400

    Update alacritty.bat

--- alacritty.bat
+++ alacritty.bat
@@ -4,5 +4,7 @@ set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\mingw64\bin\;
 set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\neovim\nvim-win64\bin\;
 set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\git\cmd\;
 set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\cmake\cmake-3.31.2-windows-x86_64\bin\;
+set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\portable\zig\zig-x86_64-windows-0.14.1\;
 
 start /B %HOMEDRIVE%%HOMEPATH%\portable\alacritty.exe --config-file %HOMEDRIVE%%HOMEPATH%\portable\alacritty\alacritty.toml
+
